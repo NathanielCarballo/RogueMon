40bb5aadea363656bdc531c5a91f9d801b9cf7d6
Nathaniel Carballo <ncarballo@bakerripley.org>
2025-08-08 14:30:23 -0500

    MVP Battle Integration: Vite proxy, backend connection, and BattleScreen UI

--- MVP/MVP_frontend/vite.config.js
+++ MVP/MVP_frontend/vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  servers: {
+    proxy: {
+      // anything starting with /api will get proxied to Flask
+      "/aoi": {
+        target: "http://localhost:5000",
+        changeOrigin: true,
+      }
+    }
+  }
 })
