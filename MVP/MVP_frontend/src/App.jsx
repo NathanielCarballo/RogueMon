ae6a371814bf4df37f919eb7aaa2cdcb10bdb449
Nathaniel Carballo <ncarballo@bakerripley.org>
2025-08-14 13:33:33 -0500

    feat(frontend): animate only turn detlas;prompt in box; grouped turn history

--- MVP/MVP_frontend/src/App.jsx
+++ MVP/MVP_frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import WelcomeScreen from "./screens/WelcomeScreen"
 import MainMenu from "./screens/MainMenu"
+import BattleScreen from "./screens/BattleScreen"
 import './App.css'
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<WelcomeScreen />} />
         <Route path="/main-menu" element={<MainMenu />} />
+        <Route path="/battle" element={<BattleScreen />} />
       </Routes>
     </Router>
   )
